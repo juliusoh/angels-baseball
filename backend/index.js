bad720d7213e172ac5a18f571cf2adf0d780d5ad
Julius Oh <juliusoh@gmail.com>
2021-10-25 16:33:00 -0700

    lol

--- backend/index.js
+++ backend/index.js
@@ -9,28 +9,16 @@ let results = [];
 fs.createReadStream(inputFile)
   .pipe(csv())
   .on('data', (data) => results.push(data))
-  .on('end', () => {
-
-  });
+  .on('end', () => {});
 
 const express = require('express');
 const app = express();
 const port = 1993;
 app.use(cors());
 
-
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
+app.get('/', (req, res) => {
+  res.send('API IS RUNNING');
+});
 
 app.get('/results', (req, res) => {
   res.send(results);
