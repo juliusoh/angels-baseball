def748b3bcccceea05c66e1de677b4a44e48f5ee
Julius Oh <juliusoh@gmail.com>
2021-10-25 16:42:05 -0700

    lol

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const express = require('express');
 const app = express();
 const port = 1993;
 app.use(cors());
-const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 );
