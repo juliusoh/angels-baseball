c913357fb1d903632e204b5f79a73ce8b53a7a3d
Julius Oh <juliusoh@gmail.com>
2021-10-25 16:19:04 -0700

    depployment

--- backend/index.js
+++ backend/index.js
@@ -20,12 +20,17 @@ app.use(cors());
 
 const __dirname = path.resolve();
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-});
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.get('/results', (req, res) => {
   res.send(results);
